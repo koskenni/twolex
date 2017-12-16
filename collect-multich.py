b8ed689436feac8f9146b2c2a7cbff8566d3510b
Kimmo Koskenniemi <kimmo.koskenniemi@helsinki.fi>
2017-12-16 15:00:33 +0200

    copyright notice added

--- collect-multich.py
+++ collect-multich.py
@@ -1,3 +1,20 @@
+# collect-multich.py
+copyright = """Copyright © 2017, Kimmo Koskenniemi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import re, csv, sys
 
 multichars = set()
@@ -6,7 +23,7 @@ features = set()
 
 def collect_multichars(str):
     if len(str) < 2: return
-    lst = re.findall(r"[{][a-zåäöVCØ]+[}]", str)
+    lst = re.findall(r"[{][a-zšžåäöA-ZØ]+[}]", str)
     for mch in lst:
         multichars.add(mch)
     return
--- lookuptest.py
+++ lookuptest.py
@@ -1,6 +1,6 @@
-"""lookuptest.py
+# lookuptest.py
 
-Copyright © 2017, Kimmo Koskenniemi
+copyright = """Copyright © 2017, Kimmo Koskenniemi
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
